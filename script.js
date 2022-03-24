6f9f952c035a39ac9cb1d39d3baa3eaf01e68119
Veronica Minciuna <veronicaminci@gmail.com>
2022-03-24 00:06:33 -0400

    light & dark mode colors

--- script.js
+++ script.js
@@ -13,9 +13,20 @@ function darkMode() {
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_feeling_proud_dark.svg';
+    image1.src = 'img/undraw_proud_coder_dark.svg';
     image2.src = 'img/undraw_feeling_proud_dark.svg';
     image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    image1.src = 'img/undraw_proud_coder_light.svg';
+    image2.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
 }
 
 //Switch Theme Dynamically
